86c14577a7ba76b2a9747411f7afa983b95d803d
Omojola Samson <omojolasamsonade@gmail.com>
2017-06-20 02:30:43 +0100

    Added Clear DB config

--- config/database.php
+++ config/database.php
@@ -41,16 +41,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_c946b5a611df608'),
+            'username' => env('DB_USERNAME', 'b0cef2b9bbb3c7'),
+            'password' => env('DB_PASSWORD', '6bbadffb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
 
